6a852530527be12d53a692e8d44e30a511a91b9d
IrwinTarma <wirwinw@gmail.com>
2017-09-02 16:49:04 -0500

    Primer formulario
    Correcto!

--- casalopez/resources/views/layouts/app.blade.php
+++ casalopez/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/form.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
