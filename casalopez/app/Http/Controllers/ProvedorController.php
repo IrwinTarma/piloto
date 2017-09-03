191f7615f20b7118c197a811795007902043ee0c
IrwinTarma <wirwinw@gmail.com>
2017-09-03 00:50:32 -0500

    form provedor
    index, store

--- casalopez/app/Http/Controllers/ProvedorController.php
+++ casalopez/app/Http/Controllers/ProvedorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\provedor;
 
 class ProvedorController extends Controller
 {
@@ -13,11 +14,17 @@ class ProvedorController extends Controller
 
     public function index()
     {
+    	//$provedor=Provedor::orderBy()
     	return view("provedor.index");
     }
 
     public function store()
     {
-    	return "guardado";
+    	return view("provedor.store");
+    }
+
+    public function show()
+    {
+    	return view("provedor.show");
     }
 }
